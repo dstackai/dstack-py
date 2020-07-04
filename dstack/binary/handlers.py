211045ecb73dc7120e7b14953d49037c5917aa37
swordhands <zinda.khud@gmail.com>
2020-07-04 22:25:23 +0300

    Add most common content types and extensions

--- dstack/binary/handlers.py
+++ dstack/binary/handlers.py
@@ -8,8 +8,7 @@ from dstack.content import CONTENT_TYPE_MAP_REVERSED
 class FileEncoder(Encoder[Path]):
     def encode(self, obj: Path, description: Optional[str], params: Optional[Dict]) -> FrameData:
         length = obj.stat().st_size
-        ext = obj.suffix[1:]
-        media_type = MediaType(CONTENT_TYPE_MAP_REVERSED.get(ext, "application/binary"))
+        media_type = MediaType(CONTENT_TYPE_MAP_REVERSED.get(obj.suffix, "application/binary"))
         f = obj.open("rb")
         buf = StreamContent(f, length)
         return FrameData(buf, media_type, description, params, {"filename": obj.name})
